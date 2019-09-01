64d801525270f5fe6ebbf2b76c8a54aeb4c41751
svey <marcus.svehlak@gmail.com>
2019-08-31 17:15:10 -0700

    updated to babel 7

--- build/index.js
+++ build/index.js
@@ -1,16 +1,12 @@
-'use strict';
+"use strict";
 
-var _modal = require('./modal');
+var _modal = _interopRequireDefault(require("./modal"));
 
-var _modal2 = _interopRequireDefault(_modal);
+var _useModal = _interopRequireDefault(require("./useModal"));
 
-var _useModal = require('./useModal');
-
-var _useModal2 = _interopRequireDefault(_useModal);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 module.exports = {
-  Modal: _modal2.default,
-  useModal: _useModal2.default
+  Modal: _modal["default"],
+  useModal: _useModal["default"]
 };
